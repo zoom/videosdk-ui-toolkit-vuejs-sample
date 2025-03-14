939833f25a40685396929768f50962fa4d0a1ff3
Joyce Ma <joyce.ma@zoom.us>
2025-03-14 15:28:13 -0700

    Remove authEndpoint

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,7 +4,7 @@
   import '@zoom/videosdk-ui-toolkit/dist/videosdk-ui-toolkit.css'
 
   var sessionContainer
-  var authEndpoint = 'http://localhost:4000'
+  var authEndpoint = ''
   var config = {
       videoSDKJWT: '',
       sessionName: 'test',
